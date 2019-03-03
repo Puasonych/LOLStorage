7e7720d14c44de74d77acbac29d0bfcc38735de2
Puasonych <basargin.erik@gmail.com>
2019-03-03 15:43:27 +0500

    Init

---
+++ LOLStorageTests/LOLStorageTests.swift
@@ -0,0 +1,34 @@
+//
+//  LOLStorageTests.swift
+//  LOLStorageTests
+//
+//  Created by Eric Basargin on 03/03/2019.
+//  Copyright © 2019 Three-man army. All rights reserved.
+//
+
+import XCTest
+@testable import LOLStorage
+
+class LOLStorageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
