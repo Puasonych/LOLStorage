559908be942062d9ded8254184aabd384b1c55e9
Kirill Saltykov <kirill.saltykov@icloud.com>
2019-06-30 20:39:28 +0500

    Fix `argument 'products' must precede` error

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@ import PackageDescription
 let package = Package(
     name: "RStorage",
     platforms: [.iOS(.v12)],
-    swiftLanguageVersions: [.v5],
     products: [
         .library(name: "RStorage", targets: ["RStorage"])
     ],
     dependencies: [],
     targets: [
         .target(name: "RStorage")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
